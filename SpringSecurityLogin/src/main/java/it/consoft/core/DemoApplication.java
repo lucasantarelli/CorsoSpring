0ee1b4d586e895577cbb05b96da450f8964a312c
l.santarelli@consoftinformatica.it <l.santarelli>
2017-07-04 19:16:42 +0200

    added two way as run

--- SpringSecurityLogin/src/main/java/it/consoft/core/DemoApplication.java
+++ SpringSecurityLogin/src/main/java/it/consoft/core/DemoApplication.java
@@ -3,10 +3,34 @@ package it.consoft.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+
+//running in application server container
+
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer{
+
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(DemoApplication.class);
+	    }
+
+	    public static void main(String[] args) throws Exception {
+	        SpringApplication.run(DemoApplication.class, args);
+	    }
+	
+	
 }
+
+
+//caso non con as container ma spring boot
+//@SpringBootApplication
+//public class DemoApplication {
+//
+//    public static void main(String[] args) {
+//		SpringApplication.run(DemoApplication.class, args);
+//	}
+//
+//}
